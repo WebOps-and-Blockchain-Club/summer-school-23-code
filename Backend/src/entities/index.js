9bc34eb58f965fe05964b86da84066a9710e07f2
Rasagnya <gudipudirasagnya@gmail.com>
2023-07-25 02:31:05 +0530

    gitignore

--- Backend/src/entities/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const events_1 = require("./events");
-const user_1 = require("./user");
-exports.default = [user_1.User, events_1.Events];
